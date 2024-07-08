053b571d0a6b1e41eba795bee0c44c0d45dde9a6
William Peña <waamirdev@gmail.com>
2024-07-07 21:16:13 -0500

    Developing the responsive navbar within the header

--- Portfolio/src/app/components/header/navbar/navbar.component.ts
+++ Portfolio/src/app/components/header/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'navbar',
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor() {}
+
+  SelectActiveLink(ListId: string, target: EventTarget | null): void {
+    if (target instanceof HTMLElement) {
+      let anchor = target as HTMLAnchorElement;
+      var ul = document.getElementById(ListId);
+      var links = ul?.getElementsByTagName('a');
+
+      if (links) {
+        for (let i = 0; i < links.length; i++) {
+          let element = links[i];
+          element.classList.remove('active');
+        }
+      }
+
+      anchor.classList.add('active');
+    }
+  }
+
+  ToggleMenu(target: EventTarget | null): void {
+    if (target instanceof HTMLElement) {
+      let imgElement = target as HTMLImageElement;
+      let menuContainer = document.getElementById('menu-container');
+      if (menuContainer?.classList.contains('hidden')) {
+        menuContainer.classList.remove('hidden');
+        imgElement.src = '../../../../assets/img/icon/white-x.png';
+      } else {
+        menuContainer?.classList.add('hidden');
+        imgElement.src = '../../../../assets/img/icon/burger-white.png';
+      }
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    let menuContainer = document.getElementById('menu-container');
+    let imgElement = document.getElementById('burger-icon') as HTMLImageElement;
+    if (window.innerWidth > 930 && imgElement) {
+      menuContainer?.classList.add('hidden');
+      imgElement.src = '../../../../assets/img/icon/burger-white.png';
+    }
+  }
 }
--- Portfolio/src/app/components/main/main/main.component.ts
+++ Portfolio/src/app/components/main/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'main',
+  selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
