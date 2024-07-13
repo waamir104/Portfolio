dcaf24f84b040200e65af5900ce670c638b1ff5c
William Peña <waamirdev@gmail.com>
2024-07-13 15:59:55 -0500

    Adding content to the front-page component

--- Portfolio/src/app/components/main/home/front-page/front-page.component.ts
+++ Portfolio/src/app/components/main/home/front-page/front-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'front-page',
   templateUrl: './front-page.component.html',
   styleUrls: ['./front-page.component.scss']
 })
-export class FrontPageComponent {
+export class FrontPageComponent implements AfterViewInit {
 
+  @ViewChild('typedOutput') typedOutput!: ElementRef;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+      const options = {
+        strings: ['front-End', 'back-end', 'web', 'desktop', 'full-stack'],
+        typeSpeed: 90,
+        startDelay: 300,
+        backSpeed: 110,
+        shuffle: false,
+        smartBackspace: true,
+        loop: true,
+        showCursor: true,
+        cursorChar: '|',
+        contentType: 'html'
+      };
+
+      const typed = new Typed(this.typedOutput.nativeElement, options);
+  }
 }
