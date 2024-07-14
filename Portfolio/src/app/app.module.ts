5280a79b7ccc56576d454c2b6a648eadf9e99b93
William Peña <waamirdev@gmail.com>
2024-07-13 22:12:24 -0500

    Adding the social media component solution

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ProjectsComponent } from './components/main/projects/projects.component
 import { MainContentComponent } from './components/main/home/main-content/main-content.component';
 import { GetInTouchComponent } from './components/main/home/get-in-touch/get-in-touch.component';
 import { AboutUsComponent } from './components/main/home/about-us/about-us.component';
+import { SocialMediaComponent } from './components/main/social-media/social-media.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AboutUsComponent } from './components/main/home/about-us/about-us.compo
     ProjectsComponent,
     MainContentComponent,
     GetInTouchComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
